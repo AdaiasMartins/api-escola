4e3879e57cd54532fdd91af1adb415ae5e877c6c
AdaiasMartins <adaias.martins@dcx.ufpb.br>
2025-04-11 17:34:49 -0300

    criando o modelo de Curso

--- escola/models.py
+++ escola/models.py
@@ -10,3 +10,15 @@ class Estudante(models.Model):
     def __str__(self):
         return self.nome
 
+class Curso(models.model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    codigo = models.CharField(max_length=10, unique=True)
+    descricao = models.CharField(max_length=100, blank=False)
+    nivel = models.CharField(max_length=1, choices = NIVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.codigo
